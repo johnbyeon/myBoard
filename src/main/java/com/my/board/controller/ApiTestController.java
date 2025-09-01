b90fbe8f700d711539ada53c346aae35d4b5c5cb
Johnbyeon <ghks0115@gmail.com>
2025-09-01 12:14:22 +0900

    신규게시글 게시글 처리완료

--- src/main/java/com/my/board/controller/ApiTestController.java
+++ src/main/java/com/my/board/controller/ApiTestController.java
@@ -1,17 +1,58 @@
 package com.my.board.controller;
 
 import com.my.board.dto.LoginDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class ApiTestController {
-    @PostMapping("/apiTest")
-    @ResponseBody
-    public LoginDto GetTest(){
 
-        return new LoginDto("jang","1111");
+    @GetMapping("/api")
+    public String apiMain() {
+        return "test/api";
+    }
+
+    @PostMapping("/apiTest")
+    //json으로 바꿔서 웹 바디에서 직접받음
+    @ResponseBody
+    public LoginDto GetTest() {
+
+        return new LoginDto("jang", "1111");
+    }
+
+    @GetMapping("/stringTest")
+    @ResponseBody
+    public String postString() {
+        return "스트링데이터 보냄";
+    }
+
+    @PostMapping("apiEntityTest")
+    //@RequestBody에서 직접받으려면 Json으로 전달해야 함
+    public ResponseEntity<String> apiEntityTest(@RequestBody LoginDto dto) {
+        String info = dto.getUserId() + "/" + dto.getPassword();
+        System.out.println(info);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(info);
+    }
+
+    @PostMapping("/mapTest")
+    @ResponseBody
+    public Map<String, String> apiMapTest(@RequestBody LoginDto dto) {
+        Map<String, String> map = new HashMap<>();
+        map.put("아이디", dto.getUserId());
+        map.put("비밀번호", dto.getPassword());
+        return map;
+
+    }
+    @DeleteMapping("delete")
+    @ResponseBody
+    public String delete(){
+        return "삭제되었습니다.";
     }
 }
